037b0d6174181ce6671005e52d30f6f0e9672717
baljoe231 <39926481+baljoe231@users.noreply.github.com>
2024-02-12 23:56:37 -0600

    Update qachecks.sql

--- qachecks.sql
+++ qachecks.sql
@@ -1,5 +1,5 @@
 with receipts as (
-  SELECT id, purchasedItemCount, totalSpent
+  SELECT id, purchased_item_count, total_spent, points_earned
   FROM
   receipts
   WHERE
@@ -10,16 +10,18 @@ with receipts as (
 , receipt_line_items as (
   SELECT 
   receipt_id 
-  , SUM(finalPrice) as totalSpent
-  , SUM(quantityPurchased) as purchasedItemCount
+  , SUM(finalPrice) as total_spent
+  , SUM(quantityPurchased) as purchased_item_count
+  , SUM(COALESCE(pointsEarned,0)) as points_earned
   FROM 
   receipt_line_items
   GROUP BY 1
 )
 
 SELECT receipts.receipt_id
-  CASE WHEN receipts.purchasedItemCount <> receipt_line_items.purchasedItemCount THEN TRUE ELSE FALSE END as purchasedItemCount_mismatch
-  , CASE WHEN receipts.totalSpent <> receipt_line_items.totalSpent THEN TRUE ELSE FALSE END as totalSpent_mismatch
+  CASE WHEN receipts.purchased_item_count <> receipt_line_items.purchased_item_count THEN TRUE ELSE FALSE END as purchased_item_count_mismatch
+  , CASE WHEN receipts.total_spent <> receipt_line_items.total_spent THEN TRUE ELSE FALSE END as total_spent_mismatch
+  , CASE WHEN receipt_line_items.points_earned > 0 AND receipt_line_items.points_earned <> receipts.points_earned THEN TRUE ELSE FALSE END as points_earned_mismatch 
 FROM
 receipts 
 JOIN receipt_line_items on receipt_line_items.receipt_id = receipts.id
